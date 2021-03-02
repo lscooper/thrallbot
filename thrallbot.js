7ab6816adc4dbe0588870663db3b28973f6d7547
Laura SC <laura.shearer.cooper@gmail.com>
2021-03-02 11:38:29 -0700

    Howdy randomized

--- thrallbot.js
+++ thrallbot.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js');
 //var auth = require('./auth.json');
 
 var client = new Discord.Client({
-   // token: auth.token,
+    // token: auth.token,
     token: process.env.TOKEN,
     autorun: true
- });
+});
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
@@ -17,20 +17,26 @@ client.on('message', message => {
         // send back "Pong." to the channel the message was sent in
         message.channel.send('Pong.');
     }
-    if (message.content === ("!bpt") ) {
+    if (message.content === ("!bpt")) {
         console.log("Nice.");
-      message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png');
-   message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png'); 
-   message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png'); 
+        message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png');
+        message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png');
+        message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png');
     }
-  if (message.content.match("bad post thrall") ) {
-      console.log("Nice2.");
-    message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png');
- message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png'); 
- message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png'); 
+    if (message.content.match("bad post thrall")) {
+        console.log("Nice2.");
+        message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png');
+        message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png');
+        message.channel.send('https://cdn.discordapp.com/attachments/244960108239388682/310537986884370432/Wow-64_2017-04-27_20-36-22-27.png');
     }
-    if (message.content.match("howdy :3")) {
-        message.channel.send('https://cdn.discordapp.com/attachments/804953498177372230/816195352989794314/image0.png');      
+    if (message.content.match("!howdy ny'all")) {
+        message.channel.send('https://cdn.discordapp.com/attachments/804953498177372230/816195352989794314/image0.png');
+    }
+    if (message.content.match("!howdy")) {
+        message.channel.send('https://cdn.discordapp.com/attachments/804953498177372230/814599387942486016/20210129_232810-ANIMATION.png');
+    }
+    if (message.content.match("!howdy spin")) {
+        message.channel.send('https://cdn.discordapp.com/attachments/804953498177372230/812960061948297306/howdy.gif');
     }
 });
 
